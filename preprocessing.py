2f1440bf3177172e9bedb2e50d6983ffb97764d4
Guillaume <34623092+GuillaumeLab@users.noreply.github.com>
2019-03-12 12:27:33 +0100

    regex pour isoler l'expérience; à améliorer

--- preprocessing.py
+++ preprocessing.py
@@ -14,4 +14,17 @@ Ce code permet de créer une nouvelle colonne "seniority" qui permet d'évaluer 
 df['Title'] = df['Title'].str.lower() # transforme la case du titre en minuscule
 List_hier =["Directeur","directrice","manager","VP","SVP","president","PDG","Director","CTO","chief","General Manager","EVP","executive","responsable région","chef","membre","board","direction","head","graduate","junior"]
 df['Seniority'] = df['Title'].str.extract("(" + "|".join(List_hier) +")", expand=False)  #extrait le titre correspondant à la List_hier (liste de titre hierarchique)
+
+
+"""
+Ce code permet de créer une nouvelle colonne "Experience" qui permet d'évaluer le l'expérience requise pour le poste
+"""
+df['Experience'] = txt.str.extract("([0-9]+)(?=\s(an |ans |years |year ))", expand=False) #: extrait tous les nombresentre 0 et 9 
+df['Experience']=df['Experience'].astype("float") #: converti les strings de nombre en chiffre
+df['Experience']=df['Experience'].loc[df['Experience']<11] # élimine les l'expérience supérieure à 10 ans
+
+""" possibilité d'amélioration 
+
+df['Experience2'] = txt.str.extract("([^.?!;\r\n]*(?<=[.?\s!;\r\n])experience(?=[\s.?!;\r\n])[^.?!;\r\n]*[.?!;\r\n])", expand=False)# élimine les l'expérience supérieure à 10 ans
+df['Experience2']
 """
