5282e0f6118662db2acc086471f2ec6575e70191
Guillaume <34623092+GuillaumeLab@users.noreply.github.com>
2019-03-26 11:22:16 +0100

    with the true_date column

--- project_version_4.py
+++ project_version_4.py
@@ -16,7 +16,41 @@ annonces = pd.read_csv('C:/Users/Administrateur/Documents/SIMPLONr/df_pymongo.cs
 # Suppression de colonnes inutiles
 annonces.drop(['index','estimated','linked','_id'], axis=1, inplace=True)
 
+annonces = annonces.drop(annonces.index[8])#empty line
+annonces = annonces.reset_index()
 
+annonces = annonces[annonces["timestamp"].notna()]
+annonces["timestamp"] = pd.to_datetime(annonces["timestamp"],infer_datetime_format=True)
+annonces = annonces.reset_index()
+
+def vraie_date(df):
+    
+    # Première fonction pour trouver l'unité de temps après "il y a"
+    unit_dic = {"heure": "h", "jour":"D", "mois":"D", "minute":"m", "second":"second"}
+    def unit_giver(jobby):
+        temp = jobby
+        for scale in unit_dic.keys():
+            if scale in temp:
+                temp = unit_dic[scale]
+        return temp
+
+    # Petite fonction pour enlever les symboles '+'
+    def plus_remover(jobby):
+        temp = jobby
+        if "+" in temp:
+            temp = temp[:-1]
+        return pd.to_numeric(temp, errors='coerce')
+
+    # Crée un tuple contenant les sorties des fonctions au-dessus pour
+    # une ligne du dataset
+    def time_diff(jobby):
+        if "mois" not in jobby[1]:
+            return plus_remover(jobby[0]), unit_giver(jobby[1])
+        return 30*plus_remover(jobby[0]), unit_giver(jobby[1])
+    
+    return df["timestamp"] - df["Date"].apply(lambda x: time_diff(x[7:].split(" "))).apply(lambda x: pd.to_timedelta(x[0], unit=x[1]))
+
+annonces["true_date"]=vraie_date(annonces)
 
 # On passe les colonnes interessantes en minuscules pour faciliter les regex
 annonces['Title'] = annonces['Title'].str.lower() 
