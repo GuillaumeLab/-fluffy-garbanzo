5897145878c8a0d8dd6d90d6e0a9aef69945aec4
CealClem <46370491+CealClem@users.noreply.github.com>
2019-04-03 14:12:19 +0200

    Update forest.py

--- forest.py
+++ forest.py
@@ -3,20 +3,36 @@ import numpy as np
 from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
+#set the random seed
+np.random.seed(0)
 
-df = pd.read_csv("/Users/Celia/Documents/simplon/MachineLearning/projet/df_pymongo_new.csv", encoding = 'utf8')
+df = pd.read_csv("/Users/Celia/Documents/simplon/MachineLearning/projet_indeed/df_pymongo_merged_with_companies.csv", encoding = 'utf8')
 
+s = df['Salaires']
+s.isnull().sum()
+#13343 
+
+#J'avais un problème avec l'accent et appostrophe dans 'niveau d'études' donc j'ai changé 
+cols = list(df.columns)
+print(cols[20])
+cols[20] = 'Niveau etudes'
+
+df.columns = cols
 #Je garde que les colonnes utiles 
-df = df[["Salaires","Experience","stage","cdi","cdd","freelance","alternance","Bassin_emploi","study","langage","Seniority","Seniority_simplified"]]
+df = df[["Salaires","Experience","stage","cdi","cdd","freelance","alternance",
+         "Bassin_emploi","Niveau etudes","langage","Seniority","Seniority_simplified","position",
+         'python','java','javascript','c+','scala']]
+#add in position !!!
 #Je converti la colonne niveau d'étude en string pour pouvoir l'encoder
-df['study'] = df['study'].astype(str)
-df = df.drop(df.index[8])#empty line
-df = df.drop(df.index[4])#empty line 
-df['Bassin_emploi'][9] = 'Paris' # Je remplace un 'ile de france' avec paris (reperé a la main)
-#df = df.dropna(how='all')# drops rows with only nans 
+df['Niveau etudes'] = df['Niveau etudes'].astype(str)
+#df = df.drop(df.index[8])#empty line
+#df = df.drop(df.index[4])#empty line 
+#df['Bassin_emploi'][9] = 'Paris' # Je remplace un 'ile de france' avec paris (reperé a la main)
+
 
 ######Pour remplir les nans de la colonne experience on va mettre la moyenne de chaque categorie "seniority"######
 #Merci guillaume :p
+#####FACULTATIVE PARCE QUE LE NOUVEAU DATASET EST DEJA TRAITÉ
 #je créé une classe mid entre junior et senior 
 df["Seniority_simplified"]=df["Seniority_simplified"].fillna("mid")
 #je remplace les valeurs nulles de expérience par la valeur "executuve","junior","mid"
@@ -35,14 +51,17 @@ for i in df["Seniority_simplified"].unique():
 df = pd.get_dummies(df)
 
 #dropping empty columns
-df = df.drop(["study_nan","study_1.0","study_8.0"], axis=1)
+df = df.drop(["Niveau etudes_nan","Niveau etudes_8.0"], axis=1)
 col_dict = {x: col for x, col in enumerate(df.columns)} 
-df = df.drop(col_dict[29], 1) #seniority responsable région (only 1 value so drop it)
+df = df.drop(col_dict[32], 1) #seniority responsable région (only 1 value so drop it)
 
-#on drop les nas (il reste 1500 lignes)
+#on drop les nas 
 dfnona = df.dropna()
+#il reste 1059 lignes with chiffre and type
+#2185 without
 X = dfnona.iloc[:, 1:]
 y = dfnona[["Salaires"]]
+
 
 #split into train and test
 from sklearn.cross_validation import train_test_split
@@ -52,18 +71,36 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, rand
 regressor = RandomForestRegressor(n_estimators=300, min_samples_split=2)
 regressor.fit(X_train, y_train)
 
+#predict
 y_pred = regressor.predict(X_test)
-
-results = regressor.fit()
-regressor.summary
+pd.DataFrame(y_pred)
 
 #on peut regarder les correlations (dunno if it's useful)
 corr = X.corr()
 
 ###HOW TO EVALUATE DIS?
 from sklearn import metrics
-print(metrics.r2_score(y_test, y_pred))#0.369117748861
-print(metrics.mean_absolute_error(y_test, y_pred))#6596.18891317
-print(metrics.mean_squared_error(y_test, y_pred))#94892109.2858
-print(metrics.median_absolute_error(y_test, y_pred))#4441.38128908
-print(metrics.explained_variance_score(y_test, y_pred))#0.370670359591
+
+from sklearn.cross_validation import GridSearchCV
+print(metrics.r2_score(y_test, y_pred))
+print(metrics.mean_absolute_error(y_test, y_pred))
+print(metrics.mean_squared_error(y_test, y_pred))
+print(metrics.median_absolute_error(y_test, y_pred))
+print(metrics.explained_variance_score(y_test, y_pred))
+
+    
+#predicting on all the dataset 
+topred = df.iloc[:,1:]
+topred.isnull().sum()
+topred.fillna(0,inplace=True)
+
+#create a list of predictions
+salaire_forest = regressor.predict(topred)
+
+#re import the dataset 
+original = pd.read_csv("/Users/Celia/Documents/simplon/MachineLearning/projet_indeed/df_pymongo_merged_with_companies.csv", encoding = 'utf8')
+#add the predictions
+original['salaire_forest'] = salaire_forest
+
+#export to csv
+original.to_csv('df_pymongo_merged_preds.csv',encoding='utf8')
